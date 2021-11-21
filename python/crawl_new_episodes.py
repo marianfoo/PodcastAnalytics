6f8bc9be67d3aca88d0fc00e818c17d74f4ae187
marianfoo <13335743+marianfoo@users.noreply.github.com>
2021-11-21 11:07:06 +0000

    feat(action): add github action to auto update episodes

---
+++ python/crawl_new_episodes.py
@@ -0,0 +1,86 @@
+import update_spotify_metadata
+import plotly.express as px
+import pandas as pd
+from pathlib import Path
+
+dict_data = {
+    'baywatchberlin' : {
+        'filename_csv' : 'list_episodes_metadata_bwb.csv',
+        'filename_other_csv' : 'list_episodes_metadata_bwb_other.csv',
+        'spotify_show_id' : '3jtLk2Zlutfjo91QZYXmlA',
+        'special_episodes' : [
+            '4VYA0hxnzjKKtL9QCHDeDr', # Trailer
+            '1KLAaERQGMmMxEtjNmojpj', # Schmitt vs Mentalist
+            '0pf0XgtnCzNAlwq7bmWTUz', # Bonus Toni Kroos
+        ],
+        'plot' : {
+            'title' : 'Baywatch Berlin',
+            'filename_image' : 'bwb_duration.png'
+        },
+        'plot_other' : {
+            'title' : 'Baywatch Berlin Spezial',
+            'filename_image' : 'bwb_duration_other.png'
+        }
+        
+    },
+    'festflauschig' : {
+        'filename_csv' : 'list_episodes_metadata_ff.csv',
+        'filename_other_csv' : 'list_episodes_metadata_ff_other.csv',
+        'spotify_show_id' : '1OLcQdw2PFDPG1jo3s0wbp',
+        'special_episodes' : [
+        ],
+        'plot' : {
+            'title' : 'Fest und Flauschig',
+            'filename_image' : 'ff_duration.png'
+        },
+        'plot_other' : {
+            'title' : 'Fest und Flauschig Spezial',
+            'filename_image' : 'ff_duration_other.png'
+        }
+
+        
+    },
+    'evds' : {
+        'filename_csv' : 'list_episodes_metadata_evds.csv',
+        'filename_other_csv' : 'list_episodes_metadata_evds_other.csv',
+        'spotify_show_id' : '3bL0TxU43Md3M384UAoIc5',
+        'special_episodes' : [
+            '7CWfRPC38OrBmm6mfCBBIb', # Trailer
+        ],
+        'plot' : {
+            'title' : 'Eulen vor die Säue',
+            'filename_image' : 'evds_duration.png'
+        },
+        'plot_other' : {
+            'title' : 'Eulen vor die Säue Spezial',
+            'filename_image' : 'evds_duration_other.png'
+        }
+        
+    }
+}
+
+def save_new_plot(csv,title,filename_image):
+    df_ff = pd.read_csv(str(Path().resolve().parent.resolve()) + '/Podcast_test/data/' + csv)
+    fig = px.bar(df_ff, x='track', y='duration_sec')
+    fig.update_layout(
+        title=title,
+        xaxis_tickfont_size=14,
+        yaxis_title='duration in seconds ',
+        xaxis_title='number show'
+    )
+    fig.write_image(str(Path().resolve().parent.resolve()) + "/Podcast_test/docs/img/" + filename_image)
+
+
+def update_metadata(show):
+    update_spotify_metadata.update_data(
+        show['spotify_show_id'],
+        show['filename_csv'],
+        show['filename_other_csv'],
+        show['special_episodes']
+    )
+
+for show in dict_data:
+    update_metadata(dict_data[show])
+    save_new_plot(dict_data[show]['filename_csv'],dict_data[show]['plot']['title'],dict_data[show]['plot']['filename_image'])
+    if len(dict_data[show]['special_episodes']) > 0 :
+        save_new_plot(dict_data[show]['filename_other_csv'],dict_data[show]['plot_other']['title'],dict_data[show]['plot_other']['filename_image'])
---
+++ python/update_spotify_metadata.py
@@ -0,0 +1,61 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+from pathlib import Path
+import csv
+import os
+
+
+def update_data(show_id,filename_csv,filename_csv_other,special_episodes):
+    client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIFY_CLIENT_ID'), client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'))
+    spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    results = spotify.show_episodes(show_id, limit=50, market='DE')
+    episodes = results['items']
+    while results['next']:
+        results = spotify.next(results)
+        episodes.extend(results['items'])
+
+    episodes_pd = []
+    episodes_other = []
+    for episode in episodes:
+        desc = episode['description'].replace(' Learn more about your ad choices. Visit podcastchoices.com/adchoices', '')
+        if any(episode['id'] in s for s in special_episodes):
+            episodes_other.append([episode['id'],episode['release_date'],episode['external_urls']['spotify'],episode['name'],episode['duration_ms']/1000,desc])
+        else:
+            episodes_pd.append([episode['id'],episode['release_date'],episode['external_urls']['spotify'],episode['name'],episode['duration_ms']/1000,desc])
+    
+        
+    df = pd.DataFrame(episodes_pd, columns = ['id', 'release_date','spotify_url', 'name','duration_sec','description'])
+    df = df.sort_values(by=['release_date'], ascending=True)
+    df = df.reset_index(drop=True)
+    tracks = []
+    for index, row in df.iterrows():
+        tracks.append(index + 1)
+    df['track'] = tracks
+    df = df[['id','track','release_date','duration_sec','spotify_url','name','description']]
+    print(df)
+    print(str(Path().resolve().parent.resolve()) + '/Podcast_test/data/' + filename_csv)
+    df.to_csv(str(Path().resolve().parent.resolve()) + '/Podcast_test/data/' + filename_csv, index = False, header=True)
+    df.to_csv(str(Path().resolve().parent.resolve()) + '/Podcast_test/docs/_data/' + filename_csv, index = False, header=True)
+
+    if len(episodes_other) > 0 :
+        df = pd.DataFrame(episodes_other, columns = ['id', 'release_date','spotify_url', 'name','duration_sec','description'])
+        df = df.sort_values(by=['release_date'], ascending=True)
+        df = df.reset_index(drop=True)
+        tracks = []
+        for index, row in df.iterrows():
+            tracks.append(index + 1)
+        df['track'] = tracks
+        df = df[['id','track','release_date','duration_sec','spotify_url','name','description']]
+        df.to_csv(str(Path().resolve().parent.resolve()) + '/Podcast_test/data/' + filename_csv_other, index = False, header=True)
+        df.to_csv(str(Path().resolve().parent.resolve()) + '/Podcast_test/docs/_data/' + filename_csv_other, index = False, header=True)
+    
+def get_metadata(filename_csv):
+    metadata = []
+    with open(filename_csv, mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            metadata.append(row)
+    return metadata
